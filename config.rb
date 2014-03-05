22db7644e8ab9df6d5ed935495a0dd8a8ff1df1e
Mike Brewington <brewington@gmail.com>
2014-03-05 14:17:24 -0500

    changes

---
+++ config.rb
@@ -0,0 +1,14 @@
+##
+## If you'd like to learn more about Sass and Compass, see the sass/README.txt
+## file for more information.
+##
+
+# Change this to :production when ready to deploy the CSS to the live server.
+environment = :production
+
+# Location of the theme's resources.
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+generated_images_dir = images_dir + "/generated"
+javascripts_dir = "js"
