2dcbd4008c73885400c0336669aad5e5779dd13b
Michael Brewington <mike@brewingtonmedia.com>
2023-11-28 15:59:27 -0500

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mikebrew.github.io",
-  base: "/dist",
+  //base: "/dist",
   integrations: [
     starlight({
       title: "My Docs",
